6d7ef0ad6f2da27fcd1690d0c296d36b108fe5f8
Marwan Mohamed Salah <marwanmohamed01552553960@gmail.com>
2025-05-25 14:44:03 +0300

    Implement initial project structure and setup

---
+++ fe/src/app/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { Button } from "@heroui/button";
+
+export default function Home() {
+  return (
+    <div className="bg-primary-40 h-screen">
+      <div className="container">
+        <h1 className="text-primary-100">Hello World</h1>
+        <Button color="primary">Button</Button>
+      </div>
+    </div>
+  );
+}
